9323103de0e4083d49117b2175c44c4ce0620457
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-09-09 16:14:29 -0500

    feat: My Sites mobile CTA points to Jetpack app
    We want to direct users to install the Jetpack app instead of the
    WordPress app, as we focus on improving the former.

--- client/my-sites/customer-home/cards/features/go-mobile/index.jsx
+++ client/my-sites/customer-home/cards/features/go-mobile/index.jsx
@@ -3,27 +3,69 @@ import { Card, Button } from '@automattic/components';
 import classnames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import { connect } from 'react-redux';
+import wpToJpImage from 'calypso/assets/images/jetpack/wp-to-jp.svg';
 import AppsBadge from 'calypso/blocks/get-apps/apps-badge';
 import CardHeading from 'calypso/components/card-heading';
 import userAgent from 'calypso/lib/user-agent';
 import { recordTracksEvent, withAnalytics } from 'calypso/state/analytics/actions';
 import { sendEmailLogin } from 'calypso/state/auth/actions';
-import { getCurrentUserEmail } from 'calypso/state/current-user/selectors';
+import { getCurrentUserEmail, getCurrentUserDate } from 'calypso/state/current-user/selectors';
 
 import './style.scss';
 
-export const GoMobile = ( { email, sendMobileLoginEmail } ) => {
+const JETPACK_APP_FOCUS_DATE = new Date( 2022, 9, 8 ).getTime();
+
+export const GoMobile = ( { isUserNewerThanJetpackAppFocus, email, sendMobileLoginEmail } ) => {
 	const translate = useTranslate();
-	const { isiPad, isiPod, isiPhone, isAndroid } = userAgent;
+	const { isDesktop, isiPad, isiPod, isiPhone, isAndroid } = userAgent;
 	const isIos = isiPad || isiPod || isiPhone;
-	const showIosBadge = ! isAndroid;
-	const showAndroidBadge = ! isIos;
-	const showOnlyOneBadge = showIosBadge !== showAndroidBadge;
 	const isDesktopApp = config.isEnabled( 'desktop' );
 
 	const emailLogin = () => {
 		sendMobileLoginEmail( email );
 	};
+
+	if ( isUserNewerThanJetpackAppFocus ) {
+		const showIosBadge = ! isAndroid && ! isDesktop;
+		const showAndroidBadge = ! isIos && ! isDesktop;
+
+		return (
+			<Card className="go-mobile go-mobile--jetpack customer-home__card">
+				<div className="go-mobile__row">
+					<img
+						className="go-mobile__icon"
+						src={ wpToJpImage }
+						width="46"
+						height="27"
+						alt="WordPress and Jetpack app"
+					/>
+					<div className="go-mobile__title">
+						<CardHeading tagName="h2">{ translate( 'Get our mobile app' ) }</CardHeading>
+						<h3 className="go-mobile__subheader">
+							{ translate( 'Everything you need to build and grow your site from any device.' ) }
+						</h3>
+					</div>
+					<div className="go-mobile__app-badges">
+						{ showIosBadge && (
+							<AppsBadge storeName={ 'ios' } utm_source={ 'calypso-customer-home' }></AppsBadge>
+						) }
+						{ showAndroidBadge && (
+							<AppsBadge storeName={ 'android' } utm_source={ 'calypso-customer-home' }></AppsBadge>
+						) }
+					</div>
+				</div>
+				{ ! showIosBadge && ! showAndroidBadge && ! isDesktopApp && (
+					<Button className="go-mobile__email-link-button is-link" onClick={ emailLogin }>
+						{ translate( 'Get the Jetpack app' ) }
+					</Button>
+				) }
+			</Card>
+		);
+	}
+
+	const showIosBadge = ! isAndroid;
+	const showAndroidBadge = ! isIos;
+	const showOnlyOneBadge = showIosBadge !== showAndroidBadge;
 
 	return (
 		<Card className="go-mobile customer-home__card">
@@ -65,6 +107,8 @@ export default connect(
 	( state ) => {
 		return {
 			email: getCurrentUserEmail( state ),
+			isUserNewerThanJetpackAppFocus:
+				new Date( getCurrentUserDate( state ) ).getTime() > JETPACK_APP_FOCUS_DATE,
 		};
 	},
 	{
